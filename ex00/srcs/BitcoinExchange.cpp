556e1be7b091b302974303a2b3e15b7cb48d4c96
samih713 <sakmohamed@gmail.com>
2024-03-24 23:08:36 +0400

    chore(BitcoinExchange): Add comments and refactoring

--- ex00/srcs/BitcoinExchange.cpp
+++ ex00/srcs/BitcoinExchange.cpp
@@ -4,21 +4,55 @@
 
 using std::runtime_error;
 
-BitcoinExchange::BitcoinExchange (const char *priceDBFile,
-								  const char *inputFile)
-	: priceDB (load_file (priceDBFile, "date,exchange_rate")),
-	  inputDB (load_file (inputFile, "date | value"))
+/* ------------------------------------------------------------------------- */
+/*                          construction/destruction                         */
+/* ------------------------------------------------------------------------- */
+
+/**
+ * @brief Constructs a BitcoinExchange object.
+ *
+ * This constructor initializes a BitcoinExchange object by loading data from the specified price
+ * file and input file. It loads the price data into the priceStream member variable and the input
+ * data into the inputStream member variable. Additionally, it calls the load_price_map() and
+ * load_input_map() functions to populate the price map and input map respectively.
+ *
+ * @param priceFile The path to the price file.
+ * @param inputFile The path to the input file.
+ * @throws runtime_error If the price file or input file fails to open.
+ */
+BitcoinExchange::BitcoinExchange (const char *priceFile, const char *inputFile)
+	: priceStream (load_file (priceFile, "date,exchange_rate")),
+	  inputStream (load_file (inputFile, "date | value"))
 {
 	load_price_map ();
-	load_input_file ();
+	load_input_map ();
 }
 
+/**
+ * @brief Destructor for the BitcoinExchange class.
+ *
+ * This destructor is responsible for cleaning up the dynamically allocated
+ * memory used by the BitcoinExchange object. It deletes the priceStream and
+ * inputStream objects.
+ */
 BitcoinExchange::~BitcoinExchange ()
 {
-	delete priceDB;
-	delete inputDB;
+	delete priceStream;
+	delete inputStream;
 }
 
+/* ------------------------------------------------------------------------- */
+/*                              member functions                             */
+/* ------------------------------------------------------------------------- */
+
+/**
+ * @brief Process the input data and calculate the total value of each transaction.
+ * The function iterates over the inputMap and for each transaction, it checks if there is a
+ * corresponding price in the priceMap. If a price is found, it calculates the total value of the
+ * transaction by multiplying the quantity with the price. If a price is not found, it searches for
+ * the nearest previous date in the priceMap and calculates the total value using that price. If an
+ * error occurs, it prints the corresponding error message.
+ */
 void BitcoinExchange::process ()
 {
 	input_m::iterator begin = inputMap.begin ();
@@ -28,18 +62,24 @@ void BitcoinExchange::process ()
 
 	for (input_m::iterator it = begin; it != end; it++)
 	{
-		// find the price for the date
+		if (ISERROR (it->first))
+		{
+			cerr << inputErrorMap[it->first] << endl;
+			continue;
+		}
+
 		price_m::iterator priceIt = priceMap.find (it->second.first);
-		if (it->second.second == INVALID_PRICE)
-			cerr << it->second.first
-				 << endl; // date here would contain the error message
-		else if (priceIt == priceMap.end ())
+		if (priceIt == priceMap.end ())
 		{
 			Date dateObj (it->second.first);
-			while (priceIt == priceMap.end ())
+			if ((--priceMap.end ())->first < dateObj) priceIt = --priceMap.end ();
+			else
 			{
-				date << --dateObj;
-				priceIt = priceMap.find (date);
+				while (priceIt == priceMap.end ())
+				{
+					date << --dateObj;
+					priceIt = priceMap.find (date);
+				}
 			}
 			cout << it->second.first << " => " << it->second.second << " = "
 				 << it->second.second * priceIt->second << endl;
@@ -50,25 +90,30 @@ void BitcoinExchange::process ()
 	}
 }
 
-ifstream *BitcoinExchange::load_file (const char   *fileName,
-									  const string &headerLine)
+/**
+ * @brief Loads a file and checks the header line.
+ *
+ * This function opens a file specified by the fileName parameter and checks if the header line
+ * matches the provided headerLine parameter. If the file fails to open or the header line is
+ * invalid, a runtime_error exception is thrown.
+ *
+ * @param fileName The name of the file to be loaded.
+ * @param headerLine The expected header line of the file.
+ * @return A pointer to an ifstream object representing the opened file.
+ * @throws runtime_error If the file fails to open or the header line is invalid.
+ */
+ifstream *BitcoinExchange::load_file (const char *fileName, const string &headerLine)
 {
 	string _headerLine;
 
 	ifstream *dataBase = new ifstream;
 	dataBase->open (fileName, std::ios_base::in);
 
-	if (!dataBase->good ())
-	{
-		throw runtime_error ("Failed to open " + string (fileName) + " .");
-	}
+	if (!dataBase->good ()) throw runtime_error ("Failed to open " + string (fileName) + " .");
 
 	std::getline (*dataBase, _headerLine);
-
 	if (_headerLine != headerLine)
-	{
 		throw runtime_error (string (fileName) + " : Invalid header line");
-	}
 
 	return dataBase;
 }
