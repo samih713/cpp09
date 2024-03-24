59fb78d180c04366a3b0ca9d089fb4fb8077a118
samih713 <sakmohamed@gmail.com>
2024-03-25 03:42:49 +0400

    chore(ex01): initial setup

---
+++ ex01/includes/RPN.hpp
@@ -0,0 +1,14 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+class RPN
+{
+
+public:
+	RPN();
+	~RPN();
+	RPN(const RPN &);
+	RPN &operator=(const RPN &);
+};
+
+#endif // RPN_HPP
---
+++ ex01/srcs/RPN.cpp
@@ -0,0 +1,23 @@
+#include "RPN.hpp"
+
+RPN::RPN()
+{
+	// constructor code
+}
+
+RPN::~RPN()
+{
+	// destructor code
+}
+
+RPN::RPN(const RPN &other)
+{
+  (void)other;
+}
+
+RPN &RPN::operator=(const RPN &other)
+{
+  (void)other;
+
+  return *this;
+}
