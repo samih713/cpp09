75005942caf547d1d0089f5ec1791d167b37a853
samih713 <sakmohamed@gmail.com>
2024-03-27 17:06:06 +0400

    feat: Implement code to use a second container

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,14 +1,64 @@
 #include "PmergeMe.hpp"
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
 #include <list>
+#include <deque>
 
 using std::cerr;
 using std::cout;
 using std::endl;
 using std::list;
+using std::pair;
+using std::deque;
 
-void   printlist_d(int_list_t l);
+template<typename T>
+void print(T &l);
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        cerr << "Usage: ./PmergeMe [sequence]" << endl;
+		return -1;
+    }
+    try
+    {
+		std::cout.setf (std::ios_base::adjustfield, std::ios_base::left);
+		// start list timer
+        list<int> _list = PmergeMe<list<int>, list<pair<int, int> > >::fill_up_list(argv + 1, argc - 1);
+		cout << std::setw(15) << "Un-Sorted: ";
+		print(_list);
+        list<int> sorted_list = PmergeMe<std::list<int>, std::list<std::pair<int, int> > >::FJMI(_list);
+		// end list timer
+		cout << std::setw(15) << "Sorted: ";
+		print(sorted_list);
+
+		// start list timer
+        deque<int> _deque = PmergeMe<deque<int>, deque<pair<int, int> > >::fill_up_list(argv + 1, argc - 1);
+		cout << std::setw(15) << "Un-Sorted: ";
+		print(_deque);
+        deque<int> sorted_deque = PmergeMe<deque<int>, deque<pair<int, int> > >::FJMI(_deque);
+		// end list timer
+		cout << std::setw(15) << "Sorted: ";
+		print(sorted_deque);
+    }
+	catch (std::runtime_error &e)
+	{
+		cout << e.what() << endl;
+	}
+	return 0;
+}
+
+template<typename T>
+void print(T &l)
+{
+    for (typename T::iterator it = l.begin(); it != l.end(); it++)
+        cout << *it << " ";
+    cout << endl;
+}
+
+#if 0
 double Random();
 
 int main()
@@ -29,14 +79,10 @@ int main()
     return 0;
 }
 
-void printlist_d(int_list_t l)
-{
-    for (int_list_t::iterator it = l.begin(); it != l.end(); it++)
-        cout << *it << " ";
-    cout << endl;
-}
+
 
 double Random()
 {
     return std::rand() % 100;
 }
+#endif // main tester
